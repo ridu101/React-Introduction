6bfe37743bbdefc66a4e726ee0535a9f9bf23342
ridu101 <ridu116540@gmail.com>
2025-11-19 23:16:02 +0600

    again

--- hshdf.js
+++ hshdf.js
@@ -507,7 +507,525 @@ friends.pop();
 // console.log(friends)
 
 // add element from the first of the array
+friends.unshift("Shihab", "Emon")//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
 friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3))
+//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3))
+//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3))
+//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3)) //  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3))
+//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3))
+//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3))
+//  let and const data type
+// variable
+let name = "ridwan"
+name = " ridwan ahmed"
+// it can be changeable
+
+const countryName = "Bangladesh"
+// it cannot be changeable
+
+// if else condition
+// single condition
+const number = 20;
+if (number >= 30) {
+     console.log("Yes")
+}
+else {
+     console.log("No")
+}
+
+// multiple condition and or or 
+// and all condition must be true
+const number1 = 35;
+if (number1 >= 30 && number1 >= 30) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+// or -- just one condition must be true
+
+const number2 = 4;
+if (number2 >= 10 || number2 < 3) {
+     // console.log("Yes")
+}
+else {
+     // console.log("No")
+}
+
+// array in Java Script
+
+const friends = ['Ridu', 'Sourav', "Mahin", "Aziz", "Himel"]
+// print the whole array
+// console.log(friends);
+// print the array length
+// console.log(friends.length);
+// print the specific index value
+// console.log(friends[2]);
+// adding element to the last of the  array
+friends.push("Tahsin", "Niloy");
+// console.log(friends)
+// remove element from the last of the array
+friends.pop();
+// console.log(friends)
+
+// add element from the first of the array
+friends.unshift("Shihab", "Emon")
+console.log(friends)
+// remove element from the first of the array
+friends.shift()
+console.log(friends)
+
+// slice and splice
+
+console.log(friends.slice(2,3))
+console.log(friends.splice(2,3)) 
 console.log(friends)
 // remove element from the first of the array
 friends.shift()
